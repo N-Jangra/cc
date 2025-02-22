bf8b01ff3b03752e2f4a4fa2111d4a9cb5f9c7cd
Nitin Jangra <itznitinjangra@gmail.com>
2025-02-22 21:49:25 +0530

    Update app.go

--- cmd/handlers/app.go
+++ cmd/handlers/app.go
@@ -15,8 +15,8 @@ import (
 func InD(c echo.Context) error {
 
 	// Fetch data from the external API
-	apiKey := "nOOmoEMK4YY8UNYpEswN4rprA0OTXFSu" // replace with your API key
-	url := fmt.Sprintf("https://calendarific.com/api/v2/holidays?api_key=%s&country=IN&year=%d", apiKey, time.Now().Year())
+	ak := os.Getenv("apikey")
+	url := fmt.Sprintf("https://calendarific.com/api/v2/holidays?api_key=%s&country=IN&year=%d", ak, time.Now().Year())
 
 	// make http request
 	res, err := http.Get(url)
